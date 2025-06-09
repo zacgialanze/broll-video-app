860a7dc818acdeed9b3fbaff336cc190211c0984
zacgialanze <zacgialanze2024@gmail.com>
2025-06-10 00:09:13 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.markdown(f"""
     [data-testid="stAppViewContainer"] {{
         background-image: url("data:image/png;base64,{encoded}");
         background-size: cover;
-        background-position: 47% top 60%;
+        background-position: 80% top 60%;
         background-repeat: no-repeat;
     }}
 
